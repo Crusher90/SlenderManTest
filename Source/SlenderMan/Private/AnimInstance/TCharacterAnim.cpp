ebab862f48b9e988614fbbd91e5ea31da8692776
Crusher90 <rahulvallecha17@gmail.com>
2025-03-13 21:08:27 +0530

    imported and added animations to player

--- Source/SlenderMan/Private/AnimInstance/TCharacterAnim.cpp
+++ Source/SlenderMan/Private/AnimInstance/TCharacterAnim.cpp
@@ -2,3 +2,25 @@
 
 
 #include "AnimInstance/TCharacterAnim.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UTCharacterAnim::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	MyCharacter = Cast<ACharacter>(TryGetPawnOwner());
+}
+
+void UTCharacterAnim::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+	if(MyCharacter == nullptr)
+		return;
+	FVector Velocity = MyCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = MyCharacter->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = MyCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+}
--- Source/SlenderMan/Public/AnimInstance/TCharacterAnim.h
+++ Source/SlenderMan/Public/AnimInstance/TCharacterAnim.h
@@ -13,4 +13,21 @@ UCLASS()
 class SLENDERMAN_API UTCharacterAnim : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fields", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<ACharacter> MyCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fields", meta = (AllowPrivateAccess = "true"))
+	double Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fields", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fields", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
 };
